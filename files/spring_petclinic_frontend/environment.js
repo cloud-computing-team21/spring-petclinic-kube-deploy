6fb8937ac285e2942938625335f7ece2c4029b71
Oriol Deiros <oriol.deiros@upc.edu>
2024-02-22 16:53:49 +0100

    All working :)

--- files/spring_petclinic_frontend/environment.js
+++ files/spring_petclinic_frontend/environment.js
@@ -1,3 +1,5 @@
-const env = {
-  REST_API_URL: 'http://spring-petclinic-rest.appsint.costaisa.org/petclinic/api/'	
-};
+export const environment = {
+    production: false,
+    REST_API_URL: "https://a4c3e1354b8c84877a692c035b94ca27-650553475.us-east-1.elb.amazonaws.com/petclinic/api/"
+  }
+  
