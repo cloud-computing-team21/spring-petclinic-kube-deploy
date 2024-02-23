9077125192d3800337860848fa6f2d76558600c1
Oriol Deiros <oriol.deiros@upc.edu>
2024-02-23 13:30:28 +0100

    Cleanup and add letsencrypt prod certs

--- files/spring_petclinic_frontend/environment.js
+++ files/spring_petclinic_frontend/environment.js
@@ -1,5 +1,5 @@
 const env = {
-  REST_API_URL: 'https://af41297e3af6a478fb0a7c86fb08608f-1444351286.us-east-1.elb.amazonaws.com/petclinic/api/'	
+  REST_API_URL: 'https://<<<<>>>>/petclinic/api/'	
 }; 
 
 
